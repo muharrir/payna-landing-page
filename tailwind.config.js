7b47f3b7be3269eef6445c2141bf69e27b10d605
Muharrir <muharrir97@gmail.com>
2023-04-10 19:45:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        whiteBlue: "#F5F6FB",
+        blueOcean: "#1F7CFF",
+        grey: "#575455",
+        red: "#F75C4E",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins"],
+    },
+  },
+  plugins: [],
+};
